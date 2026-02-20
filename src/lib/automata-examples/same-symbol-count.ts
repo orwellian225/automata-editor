28fe1cd0a3b980842c8dc1f1389a82023a2f989f
Brendan Griffiths <orwellian225@gmail.com>
2026-02-20 17:51:52 +0200

    feat: same_symbol_machine

---
+++ src/lib/automata-examples/same-symbol-count.ts
@@ -0,0 +1,140 @@
+import { dtm_type } from "$lib/automata-core/decision-turing-machine";
+import type { AutomataDescription } from "$lib/automata-core/automata-description";
+
+export const same_symbol_count_desc: AutomataDescription = {
+    name: "Same Symbol Count",
+    type: dtm_type,
+    test_cases: [],
+    automata: {
+        states: [
+            { id: 0, label: "q0", diagram: { position: { x: -150, y: 0 } } },
+            { id: 1, label: "qA", diagram: { position: { x: -300, y: 0 } } },
+            { id: 2, label: "qR", diagram: { position: { x: 300, y: 0 } } },
+            { id: 3, label: "q1", diagram: { position: { x: 0, y: -150 } } },
+            { id: 4, label: "q2", diagram: { position: { x: 0, y: 150 } } },
+            { id: 5, label: "q3", diagram: { position: { x: 100, y: 0 } } },
+        ],
+        problem_alphabet: ["0", "1"],
+        tape_alphabet: ["_", "#"],
+        transitions: [
+            {
+                curr_state_id: 0,
+                read_symbol: "_",
+                next_state_id: 1,
+                write_symbol: "_",
+                direction: +1,
+            },
+            {
+                curr_state_id: 0,
+                read_symbol: "#",
+                next_state_id: 0,
+                write_symbol: "#",
+                direction: +1,
+            },
+            {
+                curr_state_id: 0,
+                read_symbol: "0",
+                next_state_id: 3,
+                write_symbol: "#",
+                direction: +1,
+            },
+            {
+                curr_state_id: 0,
+                read_symbol: "1",
+                next_state_id: 4,
+                write_symbol: "#",
+                direction: +1,
+            },
+
+            {
+                curr_state_id: 3,
+                read_symbol: "_",
+                next_state_id: 2,
+                write_symbol: "_",
+                direction: +1,
+            },
+            {
+                curr_state_id: 3,
+                read_symbol: "#",
+                next_state_id: 3,
+                write_symbol: "#",
+                direction: +1,
+            },
+            {
+                curr_state_id: 3,
+                read_symbol: "0",
+                next_state_id: 5,
+                write_symbol: "#",
+                direction: -1,
+            },
+            {
+                curr_state_id: 3,
+                read_symbol: "1",
+                next_state_id: 3,
+                write_symbol: "1",
+                direction: +1,
+            },
+
+            {
+                curr_state_id: 4,
+                read_symbol: "_",
+                next_state_id: 2,
+                write_symbol: "_",
+                direction: +1,
+            },
+            {
+                curr_state_id: 4,
+                read_symbol: "#",
+                next_state_id: 4,
+                write_symbol: "#",
+                direction: +1,
+            },
+            {
+                curr_state_id: 4,
+                read_symbol: "0",
+                next_state_id: 4,
+                write_symbol: "0",
+                direction: +1,
+            },
+            {
+                curr_state_id: 4,
+                read_symbol: "1",
+                next_state_id: 5,
+                write_symbol: "#",
+                direction: -1,
+            },
+
+            {
+                curr_state_id: 5,
+                read_symbol: "_",
+                next_state_id: 0,
+                write_symbol: "_",
+                direction: +1,
+            },
+            {
+                curr_state_id: 5,
+                read_symbol: "#",
+                next_state_id: 5,
+                write_symbol: "#",
+                direction: -1,
+            },
+            {
+                curr_state_id: 5,
+                read_symbol: "0",
+                next_state_id: 5,
+                write_symbol: "0",
+                direction: -1,
+            },
+            {
+                curr_state_id: 5,
+                read_symbol: "1",
+                next_state_id: 5,
+                write_symbol: "1",
+                direction: -1,
+            },
+        ],
+        initial_state: 0,
+        accept_state: 1,
+        reject_state: 2,
+    },
+};
--- src/lib/automata_description.svelte.ts
+++ src/lib/automata_description.svelte.ts
@@ -4,12 +4,16 @@ import {
     automata_default,
 } from "./automata-core/automata-description";
 
-let automata_description = $state<AutomataDescription>({
-    name: "Default Decision TM",
-    test_cases: [],
-    type: dtm_type,
-    automata: automata_default(dtm_type),
-});
+import { same_symbol_count_desc } from "./automata-examples/same-symbol-count";
+
+// let automata_description = $state<AutomataDescription>({
+//     name: "Default Decision TM",
+//     test_cases: [],
+//     type: dtm_type,
+//     automata: automata_default(dtm_type),
+// });
+let automata_description = $state<AutomataDescription>(same_symbol_count_desc);
+
 export function set_automata_description(description: AutomataDescription) {
     automata_description = description;
 }
